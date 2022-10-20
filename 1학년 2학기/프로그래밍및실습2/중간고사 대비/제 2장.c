e84933452b8aad14cde619c8eb2540a249208d8b
AI-WonYJ <101448204+AI-WonYJ@users.noreply.github.com>
2022-10-20 18:43:11 +0900

    Update and rename test.py to 제 2장.c

---
+++ 1학년 2학기/프로그래밍및실습2/중간고사 대비/제 2장.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+	
+	/* Lab: 간단한 계산을 해보자
+	printf("2 + 3 = %d\n", 2 + 3);
+	print("2 - 3 = %d\n", 2 - 3);
+	printf("2 * 3 = %d\n", 2 * 3);
+	printf("2 / 3 = %d\n", 2 / 3);
+	return 0;*/
+}
