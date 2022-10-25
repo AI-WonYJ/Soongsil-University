05547dc6d304f00f39d8e5c0270ed73e5fe9817e
AI-WonYJ <101448204+AI-WonYJ@users.noreply.github.com>
2022-10-26 07:36:27 +0900

    Create 제 4장.c

---
+++ 1학년 2학기/프로그래밍및실습2/중간고사 대비/제 4장.c
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+/* Mini Project: 태양빛 도달 시간
+void main() {
+	double speed = 300000;
+	double distance = 149600000;
+	double time;
+	time = (distance / speed);
+	printf("빛의 속도는 %lfkm/s\n", speed);
+	printf("태양과 지구와의 거리 %lfkm\n", distance);
+	printf("도달 시간은 %lf초\n", time);
+}*/
