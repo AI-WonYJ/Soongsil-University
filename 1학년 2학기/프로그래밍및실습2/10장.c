85e375402378eae9945117e6c7b7d0354faaed27
AI-WonYJ <101448204+AI-WonYJ@users.noreply.github.com>
2022-11-02 14:35:14 +0900

    Update 10장.c

--- 1학년 2학기/프로그래밍및실습2/10장.c
+++ 1학년 2학기/프로그래밍및실습2/10장.c
@@ -79,3 +79,27 @@ int main(void) {
 	return 0;
 }*/
 
+
+
+//#define SIZE 10
+//
+//int main(void) {
+//	int prices[SIZE] = { 0 };
+//	int i, maximum;
+//	printf("---------------------------------------\n");
+//	printf("1 2 3 4 5 6 7 8 9 10\n");
+//	printf("---------------------------------------\n");
+//	srand((unsigned)time(NULL));
+//	for (i = 0; i < SIZE; i++) {
+//		prices[i] = (rand() % 100) + 1;
+//		printf("%.3d ", prices[i]);
+//	}
+//	printf("\n\n");
+//	maximum = prices[0];
+//	for (i = 1; i < SIZE; i++) {
+//		if (prices[i] > maximum)
+//			maximum = prices[i];
+//	}
+//	printf("최소값은 %d입니다.", maximum);
+//	return 0;
+//}
