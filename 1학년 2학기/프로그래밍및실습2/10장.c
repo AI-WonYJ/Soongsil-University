67cfca829dd3bd890c1ef8f6e0384c819be6bdd0
AI-WonYJ <101448204+AI-WonYJ@users.noreply.github.com>
2022-11-02 15:21:00 +0900

    Update 10장.c

--- 1학년 2학기/프로그래밍및실습2/10장.c
+++ 1학년 2학기/프로그래밍및실습2/10장.c
@@ -131,3 +131,62 @@ int main(void) {
 //	printf("\n%d",count);
 //		return 0;
 //}
+
+
+
+//#define SIZE 10
+//
+//int main(void) {
+//	int key, i;
+//	int count = 0;
+//	int list[SIZE] = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+//
+//	printf("탐색할 값을 입력하시오: ");
+//	scanf("%d", &key);
+//
+//	for (i = 0; i < SIZE; i++) {
+//		count++;
+//		if (list[i] == key) {
+//			printf("탐색 성공 인덱스 = %d\n탐색 횟수: %d\n", i,count);
+//		}
+//	}
+//
+//	printf("탐색 종료\n");
+//	return 0;
+//}
+
+
+
+//#define SIZE 16
+//
+//int binary_search(int list[], int n, int key);
+//
+//int main(void) {
+//	int key;
+//	int grade[SIZE] = { 2, 6, 11, 13, 18, 20, 22, 27, 29, 30, 34, 38, 41, 42, 45, 47 };
+//	printf("탐색할 값을 입력하시오: ");
+//	scanf("%d", &key);
+//	printf("탐색 결과 = %d\n", binary_search(grade, SIZE, key));
+//
+//	return 0;
+//}
+//
+//int binary_search(int list[], int n, int key) {
+//	int low, high, middle;
+//	low = 0;
+//	high = n - 1;
+//	while (low <= high) {
+//		printf("[%d %d]\n", low, high);
+//		middle = (low + high) / 2;
+//		if (key == list[middle]) {
+//			return middle;
+//		}
+//		else if (key > list[middle]) {
+//			low = middle + 1;
+//		}
+//		else {
+//			high = middle - 1;
+//		}
+//	}
+//	return -1;
+//}
