678d587dc6f98696dd5eb45e5ac83f0a1b68b88f
AI-WonYJ <101448204+AI-WonYJ@users.noreply.github.com>
2022-11-07 15:43:20 +0900

    Update 10장.c

--- 1학년 2학기/프로그래밍및실습2/10장.c
+++ 1학년 2학기/프로그래밍및실습2/10장.c
@@ -1,6 +1,7 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
+#include <Windows.h>
 
 /*#define SIZE 6
 
@@ -233,4 +234,55 @@ int main(void) {
 //		}
 //	}
 //	return total;
+//}
+
+
+
+//void display(int image[8][16])
+//{
+//	for (int r = 0; r < 8; r++) {
+//		for (int c = 0; c < 16; c++) {
+//			if (image[r][c] == 0) {
+//				printf("*");
+//			}
+//			else {
+//				Sleep(100);
+//				Beep(510, 200);
+//				Sleep(100);
+//				printf("_");
+//			}
+//		}
+//		printf("\n");
+//
+//	}
+//}
+//void inverse(int img[8][16])
+//{
+//	for (int r = 0; r < 8; r++) {
+//		for (int c = 0; c < 16; c++) {
+//			if (img[r][c] == 0)
+//				img[r][c] = 1;
+//			else
+//				img[r][c] = 0;
+//		}
+//	}
+//}
+//
+//int main(void)
+//{
+//	int image[8][16] = {
+//	{ 1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1 },
+//	{ 1,1,1,1,0,1,1,1,1,1,1,1,1,1,1,1 },
+//	{ 1,1,1,0,0,1,1,1,1,1,1,1,1,1,1,1 },
+//	{ 1,1,1,0,0,0,1,1,0,0,1,1,1,1,1,1 },
+//	{ 1,1,0,0,0,0,0,0,0,0,1,1,1,1,1,1 },
+//	{ 1,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1 },
+//	{ 1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1 },
+//	{ 1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1 } };
+//	printf("변환전 이미지\n");
+//	display(image);
+//	inverse(image);
+//	printf("\n\n변환후 이미지\n");
+//	display(image);
+//	return 0;
 //}
