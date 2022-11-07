3ba122f966d0e628dfe16567f351ea42e6b5ee04
AI-WonYJ <101448204+AI-WonYJ@users.noreply.github.com>
2022-11-07 16:11:10 +0900

    Create 11장.c

---
+++ 1학년 2학기/프로그래밍및실습2/11장.c
@@ -0,0 +1,32 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+//int main(void) {
+//	int i = 10;
+//	char c = 69;
+//	float f = 12.3;
+//
+//	printf("i의 주소: %u\n", &i);
+//	printf("c의 주소: %u\n", &c);
+//	printf("f의 주소: %u\n", &f);
+//	return 0;
+//}
+
+
+
+//int main(void) {
+//	int i = 10;
+//	double f = 12.3;
+//	int* pi = NULL;
+//
+//	double* pf = NULL;
+//	pi = &i;
+//	pf = &f;
+//
+//	printf("%u %u\n", pi, &i);
+//	printf("%u %u\n", pf, &f);
+//	return 0;
+//}
+
+
+
