ee188e88c34c19d177bd5b301dbe9bb24b1ebb92
AI-WonYJ <101448204+AI-WonYJ@users.noreply.github.com>
2022-11-09 14:56:29 +0900

    Update 11장.c

--- 1학년 2학기/프로그래밍및실습2/11장.c
+++ 1학년 2학기/프로그래밍및실습2/11장.c
@@ -94,3 +94,22 @@
 //	print_image(image);
 //	return 0;
 //}
+
+
+
+//void getSensorData(double* p) {
+//	p[0] = rand() % 100;
+//	p[1] = rand() % 100;
+//	p[2] = rand() % 100;
+//
+//	return 0;
+//}
+//
+//int main(void) {
+//	double sensorData[3];
+//	getSensorData(sensorData);
+//
+//	printf("왼쪽 센서와 장애물과의 거리: %lf\n", sensorData[0]);
+//	printf("중간 센서와 장애물과의 거리: %lf\n", sensorData[1]);
+//	printf("오른쪽 센서와 장애물과의 거리: %lf\n", sensorData[2]);
+//}
