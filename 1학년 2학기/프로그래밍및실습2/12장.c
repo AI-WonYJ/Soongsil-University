3404766116b99bb2a3415c27dd3d191699ce48a4
AI-WonYJ <101448204+AI-WonYJ@users.noreply.github.com>
2022-11-14 15:48:52 +0900

    Create 12장.c

---
+++ 1학년 2학기/프로그래밍및실습2/12장.c
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+//int main(void) {
+//	char str1[6] = "Seoul";
+//	char str2[3] = { 'i', 's' , '\0' };
+//	char str3[] = "the capital city of Korea.";
+//	printf("%s %s %s\n", str1, str2, str3);
+//	return 0;
+//}
+
+
+
+//int main(void) {
+//	char str[30] = "C language is easy";
+//	int i = 0;
+//
+//	while (str[i] != 0)
+//		i++;
+//	printf("문자열\"%s\"의 길이는 %d입니다.\n", str, i);
+//	return 0;
+//}
