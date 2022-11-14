1a1b8d107b106093ff02c23a61e18fa00975c4d0
AI-WonYJ <101448204+AI-WonYJ@users.noreply.github.com>
2022-11-14 16:42:21 +0900

    Update 12장.c

--- 1학년 2학기/프로그래밍및실습2/12장.c
+++ 1학년 2학기/프로그래밍및실습2/12장.c
@@ -37,3 +37,25 @@
 //
 //	return 0;
 //}
+
+
+
+//int main(void) {
+//	char s;
+//	printf("문자를 입력하시오: ");
+//	scanf("%c", &s);
+//	printf("아스키 코드값=%d", s);
+//}
+
+
+
+//int main(void) {
+//	char name[100];
+//	char address[100];
+//	printf("이름을 입력하시오: ");
+//	gets_s(name, 100);
+//	printf("현재 거주하는 주소를 입력하시오: ");
+//	gets_s(address, 100);
+//	puts(name);
+//	puts(address);
+//	return 0;
