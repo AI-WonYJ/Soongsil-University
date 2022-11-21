bd7d03f8f6354cc37d3303dcecb2d67653d28350
AI-WonYJ <yjason7587@naver.com>
2022-11-21 13:13:27 +0900

    2nd Revise

--- 1학년 2학기/이산수학/이산수학_심화과제/assignment.py
+++ 1학년 2학기/이산수학/이산수학_심화과제/assignment.py
@@ -5,8 +5,10 @@ def custom_linear_search(target_list, x):
     # YOU ARE ONLY ALLOWED TO MODIFY THIS ZONE - START
     x_index = -1
     for i in range(len(target_list)):
+        num_steps += 1
         if x == target_list[i]:
-            x_index = target_list[i]
+            x_index = i
+            break
 
     # YOU ARE ONLY ALLOWED TO MODIFY THIS ZONE - END
     return x_index, num_steps
--- 1학년 2학기/이산수학/이산수학_심화과제/test.py
+++ 1학년 2학기/이산수학/이산수학_심화과제/test.py
@@ -1,28 +1,42 @@
 from copy import deepcopy
 import random
 
-def custom_linear_search(target_list, x):
+def custom_binary_search(target_list, x):
     num_steps = 0
     # YOU ARE ONLY ALLOWED TO MODIFY THIS ZONE - START
-    x_index = -1
-    for i in range(len(target_list)):
-        num_steps += 1
-        if x == target_list[i]:
-            x_index = i
-            break
 
     # YOU ARE ONLY ALLOWED TO MODIFY THIS ZONE - END
     return x_index, num_steps
 
-if __name__ == '__main__':
+def custom_sort(target_list):
+    num_steps = 0
+    target_list = deepcopy(target_list)
+    # YOU ARE ONLY ALLOWED TO MODIFY THIS ZONE - START
 
-    target_list = random.sample(range(100, 10000), 10)
-    
-    print('1. Test Case -- Linear Search')
+    # YOU ARE ONLY ALLOWED TO MODIFY THIS ZONE - END
+    return target_list, num_steps
+
+if __name__ == '__main__':
+    print('2. Test Case -- Binary Search')
     x = random.choice(target_list)
-    custom_index, num_steps = custom_linear_search(target_list, x)
-    auto_index = target_list.index(x)
+    sorted_list, num_steps = custom_sort(target_list)
+    custom_index, num_steps = custom_binary_search(sorted_list, x)
+    auto_index = sorted_list.index(x)
     if custom_index == auto_index:
-        print(' PASSED ', num_steps)
+        print(' PASSED ')
+    else:
+        print(' FAILED ')
+
+    print('3. Test Case -- Sort')
+    sorted_list, num_steps = custom_sort(target_list)
+    auto_sorted_list = sorted(target_list)
+
+    equal_counter = 0
+    for val1, val2 in zip(sorted_list, auto_sorted_list):
+        if val1 == val2:
+            equal_counter += 1
+
+    if equal_counter == len(sorted_list):
+        print(' PASSED ')
     else:
         print(' FAILED ')
