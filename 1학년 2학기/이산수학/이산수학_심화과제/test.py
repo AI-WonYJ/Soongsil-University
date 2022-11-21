692055da2d2580e415393632c36b243a589b75e6
AI-WonYJ <yjason7587@naver.com>
2022-11-21 12:58:34 +0900

    1st Revise

--- 1학년 2학기/이산수학/이산수학_심화과제/test.py
+++ 1학년 2학기/이산수학/이산수학_심화과제/test.py
@@ -6,8 +6,10 @@ def custom_linear_search(target_list, x):
     # YOU ARE ONLY ALLOWED TO MODIFY THIS ZONE - START
     x_index = -1
     for i in range(len(target_list)):
+        num_steps += 1
         if x == target_list[i]:
             x_index = i
+            break
 
     # YOU ARE ONLY ALLOWED TO MODIFY THIS ZONE - END
     return x_index, num_steps
@@ -21,6 +23,6 @@ if __name__ == '__main__':
     custom_index, num_steps = custom_linear_search(target_list, x)
     auto_index = target_list.index(x)
     if custom_index == auto_index:
-        print(' PASSED ')
+        print(' PASSED ', num_steps)
     else:
         print(' FAILED ')
