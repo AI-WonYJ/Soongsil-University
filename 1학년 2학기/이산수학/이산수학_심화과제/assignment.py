29ca7695222aaecbd82da90f17674df1f4502740
AI-WonYJ <yjason7587@naver.com>
2022-11-21 23:50:44 +0900

    complete

--- 1학년 2학기/이산수학/이산수학_심화과제/assignment.py
+++ 1학년 2학기/이산수학/이산수학_심화과제/assignment.py
@@ -3,20 +3,31 @@ from copy import deepcopy
 def custom_linear_search(target_list, x):
     num_steps = 0
     # YOU ARE ONLY ALLOWED TO MODIFY THIS ZONE - START
-    x_index = -1
     for i in range(len(target_list)):
         num_steps += 1
-        if x == target_list[i]:
+        if i <= len(target_list) and x == target_list[i]:
             x_index = i
             break
-
+        else:
+            x_index = -1
     # YOU ARE ONLY ALLOWED TO MODIFY THIS ZONE - END
     return x_index, num_steps
 
 def custom_binary_search(target_list, x):
     num_steps = 0
     # YOU ARE ONLY ALLOWED TO MODIFY THIS ZONE - START
+    start, end = 0, len(target_list)
 
+    while start <= end:
+        num_steps += 1
+        half = (start + end) // 2
+        if target_list[half] == x:
+            x_index = half
+            break
+        elif target_list[half] > x:
+            end = half - 1
+        else:
+            start = half + 1
     # YOU ARE ONLY ALLOWED TO MODIFY THIS ZONE - END
     return x_index, num_steps
 
@@ -24,6 +35,10 @@ def custom_sort(target_list):
     num_steps = 0
     target_list = deepcopy(target_list)
     # YOU ARE ONLY ALLOWED TO MODIFY THIS ZONE - START
-
+    for i in range(1, len(target_list)):
+        for j in range(i, 0, -1):
+            num_steps += 1
+            if target_list[j - 1] > target_list[j]:
+                target_list[j - 1], target_list[j] = target_list[j], target_list[j - 1]
     # YOU ARE ONLY ALLOWED TO MODIFY THIS ZONE - END
     return target_list, num_steps
--- 1학년 2학기/이산수학/이산수학_심화과제/test.py
+++ 1학년 2학기/이산수학/이산수학_심화과제/test.py
@@ -4,7 +4,18 @@ import random
 def custom_binary_search(target_list, x):
     num_steps = 0
     # YOU ARE ONLY ALLOWED TO MODIFY THIS ZONE - START
+    start, end = 0, len(target_list)
 
+    while start <= end:
+        num_steps += 1
+        half = (start + end) // 2
+        if target_list[half] == x:
+            x_index = half
+            break
+        elif target_list[half] > x:
+            end = half - 1
+        else:
+            start = half + 1
     # YOU ARE ONLY ALLOWED TO MODIFY THIS ZONE - END
     return x_index, num_steps
 
@@ -12,11 +23,20 @@ def custom_sort(target_list):
     num_steps = 0
     target_list = deepcopy(target_list)
     # YOU ARE ONLY ALLOWED TO MODIFY THIS ZONE - START
-
+    for i in range(1, len(target_list)):
+        for j in range(i, 0, -1):
+            num_steps += 1
+            if target_list[j - 1] > target_list[j]:
+                target_list[j - 1], target_list[j] = target_list[j], target_list[j - 1]
     # YOU ARE ONLY ALLOWED TO MODIFY THIS ZONE - END
     return target_list, num_steps
 
 if __name__ == '__main__':
+
+    
+    target_list = random.sample(range(100, 10000), 10)
+
+
     print('2. Test Case -- Binary Search')
     x = random.choice(target_list)
     sorted_list, num_steps = custom_sort(target_list)
