51eda90c12171adefd6b34a1772456e98264454d
AI-WonYJ <yjason7587@naver.com>
2022-11-28 15:46:06 +0900

    13장

--- 1학년 2학기/프로그래밍및실습2/13장.c
+++ 1학년 2학기/프로그래밍및실습2/13장.c
@@ -85,4 +85,73 @@
 
 
 
-//struct vect
+//struct vector {
+//	float x;
+//	float y;
+//};
+//struct vector get_vector_sum(struct vector a, struct vector b);
+//int main(void)
+//{
+//	struct vector a = { 2.0, 3.0 };
+//	struct vector b = { 5.0, 6.0 };
+//	struct vector sum;
+//	sum = get_vector_sum(a, b);
+//	printf("벡터의 합은 (%f, %f)입니다.\n", sum.x, sum.y);
+//	return 0;
+//}
+//
+//
+//struct vector get_vector_sum(struct vector a, struct vector b) {
+//	struct vector result;
+//	
+//	result.x = a.x + b.x;
+//	result.y = a.y + b.y;
+//	return result;
+//}
+
+
+
+//#define STU_NUMBER 1
+//#define REG_NUMBER 2
+//struct student {
+//	int type;
+//	union {
+//		int stu_number; // 학번
+//		char reg_number[15]; // 주민등록번호
+//	} id;
+//	char name[20];
+//};
+//
+//void print(struct student s)
+//{
+//	switch (s.type)
+//	{
+//	case STU_NUMBER:
+//		printf("학번 %d\n", s.id.stu_number);
+//		printf("이름: %s\n", s.name);
+//		break;
+//	case REG_NUMBER:
+//		printf("주민등록번호: %s\n", s.id.reg_number);
+//		printf("이름: %s\n", s.name);
+//		break;
+//	default:
+//		printf("타입오류\n");
+//		break;
+//	}
+//}
+//
+//int main(void)
+//{
+//	struct student s1, s2;
+//	s1.type = STU_NUMBER;
+//	s1.id.stu_number = 20190001;
+//	strcpy(s1.name, "홍길동");
+//	s2.type = REG_NUMBER;
+//	strcpy(s2.id.reg_number, "860101-1056076");
+//	strcpy(s2.name, "김철수");
+//	print(s1);
+//	print(s2);
+//}
+
+
+
