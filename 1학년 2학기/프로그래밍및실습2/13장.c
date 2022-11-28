04ec9c8efe8ed919d0748541a0421d826b506849
AI-WonYJ <yjason7587@naver.com>
2022-11-28 15:38:05 +0900

    13장

--- 1학년 2학기/프로그래밍및실습2/13장.c
+++ 1학년 2학기/프로그래밍및실습2/13장.c
@@ -63,3 +63,26 @@
 //	struct book b1 = { 1, "바람과 함께 사라지다", "마가렛 미첼"};
 //	printf("%d, %s, %s\n", b1.id, b1.title, b1.author);
 //}
+
+
+
+//struct student {
+//	int number;
+//	char name[20];
+//	double grade;
+//};
+//int main(void)
+//{
+//	struct student s = { 20070001, "홍길동", 4.5 };
+//	struct student* p;
+//	p = &s;
+//	printf("학번=%d 이름=%s 학점=%f \n", s.number, s.name, s.grade);
+//	printf("학번=%d 이름=%s 학점=%f \n", (*p).number, (*p).name,
+//		(*p).grade);
+//	printf("학번=%d 이름=%s 학점=%f \n", p->number, p->name, p->grade);
+//	return 0;
+//}
+
+
+
+//struct vect
